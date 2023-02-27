0dc6f459e197656945bec90017dda3d1e8e6c08f
Evgeshkaa <107510236+Evgeshkaa@users.noreply.github.com>
2023-02-28 00:28:55 +0300

    feat: GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+
+    min;
+    max;
+
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.max;
     }
 
     lower() {
-
+        this.max = this.max - Math.round((this.max - this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.min + Math.round((this.max - this.min) / 2);
     }
 }
 
